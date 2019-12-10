49e6adad0c20ab856c4f13af34857a4731ed9d9c
George Wolfe <George.Wolfe@softwire.com>
2019-12-10 15:56:51 +0000

    get two bus stop IDs from a location

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import req from 'request';
 const CODE = '490010654S';
 const POSTCODE = 'N44EB'
 
-
 function getLoc(postcode, successCallback) {
     const POSTURL = 'http://api.postcodes.io/postcodes/' + postcode;
     
@@ -17,15 +16,30 @@ function getLoc(postcode, successCallback) {
         const lon = body_parsed.result.longitude;
         const lat = body_parsed.result.latitude;
     
-        successCallback({lon: lon, lat:lat});
+        successCallback({lat:lat, lon: lon});
     });
 }
 
 getLoc(POSTCODE, function (loc) {
-    console.log(loc.lat);
+    getBusID(loc, function (ids){
+        console.log(ids);
+    });
 });
 
-//https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&lat=51.574721&lon=-0.113486&app_key=2009c17b754eb17339154258424cfdca&app_id=ee46d9e0
+
+function getBusID(loc, successCallback) {
+    const IDURL = "https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&lat="  + loc.lat + "&lon=" + loc.lon + "&app_key=2009c17b754eb17339154258424cfdca&app_id=ee46d9e0" 
+    req(IDURL, function (error, response, body) {
+    
+        const body_parsed = JSON.parse(body);
+        const ids = body_parsed.stopPoints.slice(0,2).map(x => x.id);
+        
+        successCallback(ids);
+    });
+}
+
+
+
 
 
 // const BUSURL = 'https://api.tfl.gov.uk/StopPoint/' + CODE + '/Arrivals?app_id=ee46d9e0&app_key=2009c17b754eb17339154258424cfdca';
