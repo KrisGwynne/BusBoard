25cb2d569cf5b7755bed31b726cf246b4723160f
Ebony Zhang <Ebony.Zhang@softwire.com>
2019-12-11 14:27:24 +0000

    added if else

--- postcodeAPI.js
+++ postcodeAPI.js
@@ -8,22 +8,23 @@ export default class PostcodeAPI {
         const POSTURL = 'http://api.postcodes.io/postcodes/' + postcode;
 
         req(POSTURL, function (error, response, body) {
-            
+
             const body_parsed = JSON.parse(body)
 
             if (body_parsed.status !== 200) {
-                errorCallback(new Error(body_parsed.status,body_parsed.error))
+                errorCallback(new Error(body_parsed.status, body_parsed.error))
+            } else {
+
+                const lon = body_parsed.result.longitude;
+                const lat = body_parsed.result.latitude;
+
+                successCallback({
+                    lat: lat,
+                    lon: lon
+                });
             }
 
-            const lon = body_parsed.result.longitude;
-            const lat = body_parsed.result.latitude;
 
-            successCallback({
-                lat: lat,
-                lon: lon
-            });
-
-            
         });
     }
 }
--- tflAPI.js
+++ tflAPI.js
@@ -6,7 +6,7 @@ import Error from './error.js'
 export default class TflAPI {
     constructor() {}
 
-    static getBusID(loc, successCallback,errorCallback) {
+    static getBusID(loc, successCallback, errorCallback) {
         const IDURL = "https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&lat=" + loc.lat + "&lon=" + loc.lon + "&app_key=2009c17b754eb17339154258424cfdca&app_id=ee46d9e0"
         req(IDURL, function (error, response, body) {
 
@@ -14,20 +14,17 @@ export default class TflAPI {
 
             if (response.statusCode !== 200) {
                 errorCallback(new Error(response.statusCode, response.statusMessage));
-            }
-
-            
-            if (body_parsed.stopPoints.length === 0) {
+            } else if (body_parsed.stopPoints.length === 0) {
                 errorCallback(new Error(404, 'No Nearby Busstop'));
+            } else {
+                const ids = body_parsed.stopPoints.slice(0, 2).map(x => x.id);
+                successCallback(ids);
             }
 
-            const ids = body_parsed.stopPoints.slice(0, 2).map(x => x.id);
-
-            successCallback(ids);
         });
     }
 
-    static getNextBuses(ids, successCallback,errorCallback) {
+    static getNextBuses(ids, successCallback, errorCallback) {
 
         let count = 0;
         let busStopList = [];
@@ -42,13 +39,12 @@ export default class TflAPI {
 
                 if (response.statusCode !== 200) {
                     errorCallback(new Error(response.statusCode, response.statusMessage));
-                }
-    
-                let busStop = BusStop.makeStop(id, body_arr);
-                busStopList.push(busStop);
-
-                if (++count === ids.length) {
-                    successCallback(busStopList);
+                } else {
+                    let busStop = BusStop.makeStop(id, body_arr);
+                    busStopList.push(busStop);
+                    if (++count === ids.length) {
+                        successCallback(busStopList);
+                    }
                 }
             });
         });
